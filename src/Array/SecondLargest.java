89d735ebada4398f5b0422e1513ba9b9d4398329
SUMAN1040 <uemj2003@gmail.com>
2025-04-01 22:59:05 +0530

    problem 1

--- src/Array/SecondLargest.java
+++ src/Array/SecondLargest.java
@@ -6,5 +6,19 @@
 
 package Array;
 
-public class SecondLargest {
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr){
+            if (num > largest){
+                secondLargest = largest;
+                largest = num;
+            } else if ( num > secondLargest && num < largest){
+                secondLargest = num;
+            }
+        }
+        return (secondLargest == Integer.MIN_VALUE)? -1: secondLargest;
+    }
 }
