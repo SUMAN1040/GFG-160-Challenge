7e3e2cdf971e7c60a996471b1fbfc4dbbfd5dd66
SUMAN1040 <uemj2003@gmail.com>
2025-04-07 20:55:36 +0530

    Update

--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -13,12 +13,9 @@ class Solution4 {
         d = d % n;
 
         reverse(arr, 0, d - 1);
-
         reverse(arr, d, n - 1);
-
         reverse(arr, 0, n - 1);
     }
-
     static void reverse(int arr[], int start, int end) {
         while (start < end) {
             int temp = arr[start];
