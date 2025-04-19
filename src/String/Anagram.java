1aece12f64c48508d5338a60665bca583e2754c6
SUMAN1040 <uemj2003@gmail.com>
2025-04-19 22:46:00 +0530

    Add 16th Questions

--- src/String/Anagram.java
+++ src/String/Anagram.java
@@ -1,4 +1,32 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
 package String;
 
-public class Anagram {
+class Solution3 {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            freq[s2.charAt(i) - 'a']--;
+        }
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
