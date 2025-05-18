26a315208dc8bc997867989b1531f74a714e6678
SUMAN1040 <uemj2003@gmail.com>
2025-05-18 23:26:04 +0530

    Add 44th Questions

---
+++ src/Hashing/FindAllTripletswithZeroSum.java
@@ -0,0 +1,25 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Hashing;
+
+public List<List<Integer>> findTriplets(int[] arr) {
+    List<List<Integer>> res = new ArrayList<>();
+    int n = arr.length;
+
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (arr[i] + arr[j] + arr[k] == 0) {
+                    res.add(Arrays.asList(i, j, k));
+                }
+            }
+        }
+    }
+
+    return res;
+}
+}
