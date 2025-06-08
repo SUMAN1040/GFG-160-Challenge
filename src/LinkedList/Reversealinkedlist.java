8ce43c53a6b7c739491802c8a54473d8b9284fa1
suman1040 <uemj2003@gmail.com>
2025-06-08 22:05:00 +0530

    Add 65th Questions

---
+++ src/LinkedList/Reversealinkedlist.java
@@ -0,0 +1,37 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package LinkedList;
+
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
