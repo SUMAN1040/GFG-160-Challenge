a7021c1557b83cf0b55878f2460fc947a4d80411
suman1040 <uemj2003@gmail.com>
2025-06-09 23:04:59 +0530

    Add 66th Questions

---
+++ src/LinkedList/RotateaLinkedList.java
@@ -0,0 +1,48 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package LinkedList;
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution {
+    public Node rotate(Node head, int k) {
+        // add code here
+        if (head == null || head.next == null || k == 0) return head;
+
+        int length = 1;
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+        if (k == 0) return head;
+
+        Node curr = head;
+        for (int i = 1; i < k; i++) {
+            curr = curr.next;
+        }
+
+        Node newHead = curr.next;
+        curr.next = null;
+        tail.next = head;
+
+        return newHead;
+    }
+}
