03e52b748420c387f99dc8d44d25868ae939f469
suman1040 <uemj2003@gmail.com>
2025-06-28 20:55:40 +0530

    Add 83th Questions

---
+++ src/Tree/MirrorTree.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Tree;
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    void mirror(Node node) {
+        if (node == null) return;
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        mirror(node.left);
+        mirror(node.right);
+    }
+}
