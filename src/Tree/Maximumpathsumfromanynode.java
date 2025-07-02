32612379f669e5c4d17a54e5e5ba61f3f405ea32
suman1040 <uemj2003@gmail.com>
2025-07-02 23:41:33 +0530

    Add 87th Questions

---
+++ src/Tree/Maximumpathsumfromanynode.java
@@ -0,0 +1,38 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Tree;
+
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution {
+    int maxSum;
+
+    int findMaxSum(Node node) {
+        maxSum = Integer.MIN_VALUE;
+        dfs(node);
+        return maxSum;
+    }
+
+    int dfs(Node node) {
+        if (node == null) return 0;
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+        maxSum = Math.max(maxSum, left + right + node.data);
+        return node.data + Math.max(left, right);
+    }
+}
