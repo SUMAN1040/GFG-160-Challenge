5790b33e591ffe8fbb08ecce535749a6936358c1
suman1040 <uemj2003@gmail.com>
2025-07-04 21:32:15 +0530

    Add 89th Questions

---
+++ src/Tree/CheckforBST.java
@@ -0,0 +1,19 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Tree;
+
+class Solution {
+    boolean isBST(Node root) {
+        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    boolean isBST(Node node, int min, int max) {
+        return node == null || (node.data > min && node.data < max &&
+                isBST(node.left, min, node.data) &&
+                isBST(node.right, node.data, max));
+    }
+}
