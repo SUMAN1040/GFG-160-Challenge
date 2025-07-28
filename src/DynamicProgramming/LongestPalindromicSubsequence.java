720466174b6c9a961761b65111b4e92a433ab289
suman1040 <uemj2003@gmail.com>
2025-07-28 14:13:24 +0530

    Add 113th Questions

---
+++ src/DynamicProgramming/LongestPalindromicSubsequence.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package DynamicProgramming;
+
+// User function Template for Java
+
+class Solution {
+    public int longestPalinSubseq(String s) {
+        // code here
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
