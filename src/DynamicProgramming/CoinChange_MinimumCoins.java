64bb7bea0dd96f87ce64cd01a01023c83f0b08ac
suman1040 <uemj2003@gmail.com>
2025-08-05 22:34:45 +0530

    Add 121th Questions

---
+++ src/DynamicProgramming/CoinChange_MinimumCoins.java
@@ -0,0 +1,27 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package DynamicProgramming;
+
+class Solution {
+
+    public int minCoins(int coins[], int sum) {
+        // code here
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, sum + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= sum; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[sum] > sum ? -1 : dp[sum];
+    }
+}
