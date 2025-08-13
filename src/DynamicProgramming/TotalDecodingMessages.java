d3a1c306cb95555042fa2ad82b39ccfd0dc50209
suman1040 <uemj2003@gmail.com>
2025-08-14 00:21:00 +0530

    Add 129th Questions

---
+++ src/DynamicProgramming/TotalDecodingMessages.java
@@ -0,0 +1,37 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package DynamicProgramming;
+
+// User function Template for Java
+class Solution {
+    public int countWays(String digits) {
+        // code here
+        if (digits.length() == 0 || digits.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = digits.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = Integer.parseInt(digits.substring(i - 1, i));
+            int twoDigits = Integer.parseInt(digits.substring(i - 2, i));
+
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
