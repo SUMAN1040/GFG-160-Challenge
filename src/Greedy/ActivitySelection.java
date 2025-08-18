d1cae6c20b2fcc7d7914a0bd9f101872a4ac3a75
suman1040 <uemj2003@gmail.com>
2025-08-18 23:04:34 +0530

    Add 134th Questions

---
+++ src/Greedy/ActivitySelection.java
@@ -0,0 +1,28 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Greedy;
+
+
+class Solution {
+    public int activitySelection(int[] start, int[] finish) {
+        // code here
+        int[][] arr=new int[start.length][2];
+        for(int i=0;i<start.length;i++){
+            arr[i]=new int[]{start[i],finish[i]};
+        }
+        Arrays.sort(arr,(a,b)->Integer.compare(a[1],b[1]));
+        int end=-1;
+        int count=0;
+        for(int i=0;i<start.length;i++){
+            if(end<arr[i][0]){
+                count++;
+                end=arr[i][1];
+            }
+        }
+        return count;
+    }
+}
