8a91b1df1f756ccd90a3a61c230543b98e597037
suman1040 <uemj2003@gmail.com>
2025-08-30 23:48:58 +0530

    Add 144th Questions

---
+++ src/Graph/DirectedGraphCycle.java
@@ -0,0 +1,50 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Graph;
+
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] visited = new boolean[V];
+        boolean[] recStack = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, adj, visited, recStack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, boolean[] recStack) {
+        visited[node] = true;
+        recStack[node] = true;
+
+        for (int nei : adj.get(node)) {
+            if (!visited[nei]) {
+                if (dfs(nei, adj, visited, recStack)) {
+                    return true;
+                }
+            } else if (recStack[nei]) {
+                return true;
+            }
+        }
+
+        recStack[node] = false;
+        return false;
+    }
+}
