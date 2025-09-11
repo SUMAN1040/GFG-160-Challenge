a04d72d5b8d5ac7111f1c1bfc895e67b601f4cae
suman1040 <uemj2003@gmail.com>
2025-09-11 22:51:15 +0530

    Add 150th Questions

---
+++ src/Graph/AlienDictionary.java
@@ -0,0 +1,56 @@
+/*
+ * Name: Suman Kumar Dey
+ * Github: SUMAN1040
+ * Linkedln: www.linkedin.com/in/suman1040
+ */
+
+package Graph;
+
+class Solution {
+    public String findOrder(String[] words) {
+        // code here
+        Map<Character, List<Character>> graph = new HashMap<>();
+        Map<Character, Integer> indegree = new HashMap<>();
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                graph.putIfAbsent(c, new ArrayList<>());
+                indegree.putIfAbsent(c, 0);
+            }
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            String w1 = words[i];
+            String w2 = words[i + 1];
+            if (w1.length() > w2.length() && w1.startsWith(w2)) return "";
+            int minLen = Math.min(w1.length(), w2.length());
+            for (int j = 0; j < minLen; j++) {
+                char c1 = w1.charAt(j);
+                char c2 = w2.charAt(j);
+                if (c1 != c2) {
+                    graph.get(c1).add(c2);
+                    indegree.put(c2, indegree.get(c2) + 1);
+                    break;
+                }
+            }
+        }
+
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : indegree.keySet()) {
+            if (indegree.get(c) == 0) queue.add(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char curr = queue.poll();
+            sb.append(curr);
+            for (char nei : graph.get(curr)) {
+                indegree.put(nei, indegree.get(nei) - 1);
+                if (indegree.get(nei) == 0) queue.add(nei);
+            }
+        }
+
+        if (sb.length() != indegree.size()) return "";
+        return sb.toString();
+    }
+}
